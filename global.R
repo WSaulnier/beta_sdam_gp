0921e001ec329eab3e02ec23ae9a8e1fda1aa418
Duy Nguyen <duyn@SCCWRP2k.lan>
2021-11-14 16:21:45 -0800

    Fixed a small bug. At this point, the app should be in great shape

--- global.R
+++ global.R
@@ -348,7 +348,7 @@ snowdom <- function(lat, lon){
         canrun = T,
         msg = glue::glue( 
           HTML(
-            "<strong>Warning</strong>: Site is outside the Western Mountains. Classifications for the Beta SDAM WM are presented for informational purposes only. <br> The site is {outwm_sno_or_no} <br> {outwm_msg}"
+            "<strong>Warning</strong>: Site is outside the Western Mountains. Classifications for the Beta SDAM WM are presented for informational purposes only. <br> {outwm_msg}"
           )
         ),
         mod = character(0)
